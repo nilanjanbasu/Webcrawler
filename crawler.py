64ec9336cdf18a2ecb2d6b30aa4f24f6bc935298
Nilanjan <neelpulse@gmail.com>
2012-12-24 23:49:19 +0530

    Working with a few bugs

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import Queue
 import threading
 import BeautifulSoup
 import urllib2
@@ -8,6 +9,8 @@ import logging
 import socket
 from lib.TimedQueue import TimedQueue, BotRequest, HostURLParse, SafeDict, SafeSet
 from robotparser import RobotFileParser
+import sys
+import re
 import os
 
 hosts = ["http://yahoo.com", "http://google.com", "http://amazon.com",
@@ -17,13 +20,14 @@ hosts = ["http://yahoo.com", "http://google.com", "http://amazon.com",
 
 class Worker(threading.Thread):
 
-    def __init__(self, queue, shutdown_event, hash_set, workspace, robotdict):
+    def __init__(self, queue, shutdown_event, hash_set, static_set, workspace, robotdict):
         threading.Thread.__init__(self)
         self.queue = queue
         self.shutdown_event = shutdown_event
         self.hash_set = hash_set
         self.workspace = workspace
         self.robotdict = robotdict
+        self.static_set = static_set
 
     def run(self):
         while not self.shutdown_event.is_set():
@@ -66,20 +70,36 @@ class Worker(threading.Thread):
 
         if cached_parser.can_fetch('*', host. get_url()):
             print 'Going to fetch:', host.get_url()
-            bot_req = BotRequest(host.get_url())
-            try:
-                url = urllib2.urlopen(bot_req)
-            except urllib2.HTTPError as e:
-                logging.info("HttpError: Response:%d Host: %s" % (e. code, host))
-                return None
-            except urllib2.URLError as e:
-                logging.info("Server Unreachable: Reason %s" % (e.reason, ))
-                return None
-            else:
-                return url.read()  # return the HTML
+            return self.fetch_file(host.get_url())
+            #bot_req = BotRequest(host.get_url())
+            #try:
+                #url = urllib2.urlopen(bot_req)
+            #except urllib2.HTTPError as e:
+                #logging.info("HttpError: Response:%d Host: %s" % (e. code, host))
+                #return None
+            #except urllib2.URLError as e:
+                #logging.info("Server Unreachable: Reason %s" % (e.reason, ))
+                #return None
+            #else:
+                #return url.read()  # return the HTML
         else:
             logging.info("Forbidden by Robots.txt")
             return None
+
+    def fetch_file(self, url):
+
+        request = BotRequest(url)
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.HTTPError as e:
+            logging.info("HttpError: Response:%d Host: %s" % (e. code, url))
+            return None
+        except urllib2.URLError as e:
+            logging.info("Server Unreachable: URL: %s  Reason %s" % (url, e.reason))
+            return None
+        else:
+            return response.read()  # return the HTML
+
 
     def parse_and_enque(self, host, html):
         soup = BeautifulSoup.BeautifulSoup(html)
@@ -94,17 +114,81 @@ class Worker(threading.Thread):
                 self.queue.put(urlhostp)
                 self.hash_set.add(h)
             a['href'] = os.path.join(self. workspace, urlhostp. get_diskrelpath())
+        self.save_static_files(host,soup)
         return str(soup)
 
-    def save_file(self, host, html):
+    def save_static_files(self,host,soup):
+        css_files = soup.findAll('link', {'rel' : re.compile(r'stylesheet')}, href=True)
+        img_files = soup.findAll('img', src=True)
+        js_files = soup.findAll('script', src=True)
+
+        self.save_static_by_type(host, soup, css_files, 'href')
+        self.save_static_by_type(host, soup, img_files, 'src')
+        self.save_static_by_type(host, soup, js_files, 'src')
+        #all_links = css_files + img_files
+
+        #q = Queue.Queue()
+        #for link in all_links:
+            #static_url_obj = HostURLParse(link, host.get_url())
+            #if not self.static_set.in_set(static_url_obj.get_url_hash()):
+                #self.static_set.add(static_url_obj.get_url_hash())
+                #q.put(static_url_obj)
+            #else:
+                #logging.info('Found Static file: ' + static_url_obj.get_url())
+
+        #stop_event = threading.Event()
+        #if not q.empty():
+            #for i in range(5):
+                #t = threading.Thread(target=self.static_worker, args=(q,stop_event))
+                #t.setDaemon(True)
+                #t.start()
+
+            #q.join()
+            #stop_event.set()
+
+    def save_static_by_type(self, host, soup, link_items, attr):
+        q = Queue.Queue()
+        for link in link_items:
+            print host.get_url()
+            static_url_obj = HostURLParse(link[attr], host.get_url())
+            if not self.static_set.in_set(static_url_obj.get_url_hash()):
+                #self.static_set.add(static_url_obj.get_url_hash())
+                q.put(static_url_obj)
+                link[attr] = os.path.join(self. workspace, static_url_obj. get_diskrelpath())
+            else:
+                logging.info('Found Static file in cache: ' + static_url_obj.get_url())
+
+        stop_event = threading.Event()
+        if not q.empty():
+            for i in range(5):
+                t = threading.Thread(target=self.static_worker, args=(q,stop_event))
+                t.setDaemon(True)
+                t.start()
+
+            q.join()
+            stop_event.set()
+
+    def static_worker(self, static_queue, stop_event):
+
+        while not stop_event.is_set():
+            url_obj = static_queue.get()
+            content = self.fetch_file(url_obj.get_url())
+            if content:
+                self.static_set.add(url_obj.get_url_hash())
+                logging.info("Saved static file: " + url_obj.get_url())
+                self.save_file(url_obj, content)
+            static_queue.task_done()
+
+    def save_file(self, host, content):
         filepath = os.path.join(self.workspace, host.get_diskrelpath())
+        print filepath
         if not os.path.exists(os.path.dirname(filepath)):
             os.makedirs(os.path.dirname(filepath))
         try:
             f = open(filepath, 'w')
         except IOError:
             logging.error('Unable to create file: ' + filepath)
-        f.write(html)
+        f.write(content)
 
 
 class Controller:
@@ -116,6 +200,7 @@ class Controller:
         self.queue = TimedQueue(delay=req_delay)
         self.shutdown_event = threading.Event()
         self.downloaded_hash_set = SafeSet()
+        self.static_set = SafeSet()
         self.max_conn = max_conn
         self.project_directory = workspace  # take this argument from command line
         self.url_obj = url_obj
@@ -124,7 +209,7 @@ class Controller:
     def start(self):
         self.workers = []
         for i in range(self.max_conn):
-            t = Worker(self.queue, self.shutdown_event, self.downloaded_hash_set, self.project_directory, self.robots_dict)
+            t = Worker(self.queue, self.shutdown_event, self.downloaded_hash_set, self.static_set, self.project_directory, self.robots_dict)
             t.setDaemon(True)
             t.start()
             self.workers.append(t)
@@ -153,8 +238,12 @@ class Controller:
 if __name__ == '__main__':
     #host = HostURLParse('http://python.org/', '')
 
-    host = HostURLParse('http://127.0.0.1:8080/index.html', '')
+    if len(sys.argv) < 2:
+        print "Usage: ./crawler.py <Website URL>"
+        sys.exit(0)
+
+    host = HostURLParse(sys.argv[1], '')
     p = os.path.join(os.getcwd(), 'workspace/')
-    ctrl = Controller(60, 2, p, host, 5)
+    ctrl = Controller(60, 25, p, host, 5)
     ctrl.start()
 
